8c38fd622f4499c452c17f3fd527b0302da774ab
Krunal <Krunalofficial11j@gmail.com>
2025-02-12 14:20:03 +0530

    Functions Challenges ✅
    Create Folder JavaScript Challenges and add JavaScript Challenges Code
    
    - Add Two Numbers
    - Subtract Two Numbers
    - Multiplies Two Numbers

---
+++ JavaScript Challenges/Functions/02_Subtract_Two_Numbers.js
@@ -0,0 +1,13 @@
+/*
+You have a basket of apples, but you decide to give some away to your neighbor. How many apples are
+left in your basket?
+
+Challenge:
+
+Write a function that subtracts the number of apples given away from the total apples you started with,
+then returns the remaining apples.
+*/
+
+function remainingApp1es(totalApples, givenAway) {
+    return totalApples - givenAway;
+}
