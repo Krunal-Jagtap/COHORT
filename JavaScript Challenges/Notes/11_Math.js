ba6486571f6376197e471acf888bbad5e4716923
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-03-04 01:41:59 +0530

    Math

---
+++ JavaScript Challenges/Notes/11_Math.js
@@ -0,0 +1,41 @@
+// Math - work with ( numbers ✅ ) / ( bigint ❌ )
+
+// abs - Distent between 0
+console.log(Math.abs(5));
+console.log(Math.abs(-5));
+
+// round
+console.log(Math.round(2.1));
+console.log(Math.round(2.4));
+console.log(Math.round(2.5));
+console.log(Math.round(2.6));
+console.log(Math.round(2.9));
+
+// ceil ⬆
+console.log(Math.ceil(2.0));
+console.log(Math.ceil(2.1));
+console.log(Math.ceil(2.5));
+console.log(Math.ceil(2.9));
+
+// floor ⬇
+console.log(Math.floor(2.0));
+console.log(Math.floor(2.1));
+console.log(Math.floor(2.5));
+console.log(Math.floor(2.9));
+
+// trunc - ignore number after .
+console.log(Math.trunc(2.9));
+
+console.log(Math.trunc(-2.9));
+console.log(Math.floor(-2.9));
+
+// pow
+console.log(Math.pow(2, 4));
+console.log(2 ** 4);
+
+// sqrt
+console.log(Math.sqrt(25));
+
+// random ( here number 10 not include )
+console.log(Math.random());
+console.log(Math.round(Math.random() * 10));
