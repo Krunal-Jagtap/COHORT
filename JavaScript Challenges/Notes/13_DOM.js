c250291c4ae1061e243f226bb8ece0af2afa97b5
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-03-04 16:26:14 +0530

    Create 13_DOM.js

---
+++ JavaScript Challenges/Notes/13_DOM.js
@@ -0,0 +1,47 @@
+// DOM - Document Object Model
+
+// DOM Properties -
+// - document
+// - getElementById()
+// - getElementsByClassName()
+// - getElementsByTagName()
+// - querySelector()
+// - querySelectorAll()
+// - innerHTML
+// - textContent
+// - style
+
+// DOM Methods -
+// - createElement(tagName)
+// - appendChild()
+// - removeChild()
+// - addEventListener(event, function)
+// - removeEventListener(event, function)
+// - setAttribute(name, value)
+// - getAttribute(name)
+// - parentNode / parentElement
+// - childNodes / children
+// - firstChild / firstElementChild
+// - lastChild / lastElementChild
+// - nextSibling / nextElementSibling
+// - previousSibling / previousElementSibling
+// - closest(selector)
+
+// CRUD -
+// createElement(tagName) - Create new HTML element
+
+// appendChild(node) - add element
+
+// removeChild(node) - Remove element
+
+// addEventListener(event, function) - Handel events
+
+// removeEventListener(event, function) - Remove events
+
+// setAttribute(name, value) - Set value in attribute element
+
+// getAttribute(name) - Get value in attribute element
+
+// innerHTML
+
+// textContent
