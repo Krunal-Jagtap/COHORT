7b336ed5e49d478077d513dc9b811ce3526dbce5
Krunal-Jagtap <Krunalofficial11j@gmail.com>
2025-03-04 14:00:48 +0530

    Create 12_BOM.js

---
+++ JavaScript Challenges/Notes/12_BOM.js
@@ -0,0 +1,26 @@
+// BOM - Browser Object Model
+
+// Object - Global Window
+
+// console - Log Messages -
+// - log()
+// - warn()
+// - error()
+
+// innerWidth and innerHeight - Show Width and Height from browser content area
+
+// scrollX and scrollY - Show how much scrolled in pixels
+
+// localStorage - Data Key-Value pairs
+
+// alert() - Show popUp in browser with alert message
+
+// confirm() - Show popUp gives 2 option OK return true, Cancel return False
+
+// prompt() - Show popUp gives textarea, OK, Cancel
+
+// navigator - Show Information about browser
+
+// localStorage, sessionStorage - Show storing data
+
+// etc
